dd25b5e35e5e5548b2176624532656d9a7657485
Luisana Romero <lerm_172004@yahoo.com>
2013-06-16 10:19:16 -0430

    clase principal github
    agregue los arrays de la lista de clases a utilizar: autor, discos,
    libros, peliculas
    llame el menu principal
    Menu del artista o autor

--- src/github/Artista.java
+++ src/github/Artista.java
@@ -3,11 +3,97 @@
  * and open the template in the editor.
  */
 package github;
-
+import java.io.*;
+import java.util.*;
 /**
  *
  * @author Luisana
  */
-public class Artista {
+public class Artista
+{
+    private String nombre;    
+    private int añoNacimiento;       
+    private char resp='S';   
+    //Contructor sin parametros de la clase Artista
+    public Artista(){}
     
+    //Constructor con parametros de la clase Artista
+    public Artista(String nom, int ano)
+    {
+        this.nombre = nom;
+        this.añoNacimiento = ano;
+    }
+    
+    //Funcion para obtener el ano de nacimiento del autor
+    public int getAnoNacimiento()
+    {
+        return this.añoNacimiento;
+    }
+    
+    //Funcion para almacenar el ano de nacimiento del autor
+    public void setAnoNacimiento(int ano)
+    {
+        this.añoNacimiento = ano;
+    }
+    
+    //Funcion para obtener el nombre del autor
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+    
+    //Funcion para guardar el nombre del autor
+    public void setNombre(String nom)
+    {
+        this.nombre = nom;
+    }
+    
+    //Funcion para registar autor
+    public void registrautor(List<Artista> autores) throws IOException
+    {
+       String nom;
+       int ano=0;
+       BufferedReader entrada = new BufferedReader(new InputStreamReader (System.in));
+       
+       if(autores == null){
+           autores = new ArrayList<Artista>();
+       }              
+       do{
+            System.out.print("Ingrese el nombre del autor: ");
+            nom = entrada.readLine();
+            System.out.print("Ingrese el año de nacimiento del autor: ");
+            try {
+                ano = Integer.parseInt(entrada.readLine());
+            } catch (NumberFormatException ex) {
+            System.out.println("  Error el año de nacimiento, intentelo de nuevo.");
+            }
+            Artista artista = new Artista(nom,ano);
+            autores.add(artista);
+            System.out.println("Desea agregar otro artista? S/N");
+            resp = entrada.readLine().toUpperCase().charAt(0);
+       } while(resp=='S');       
+    }
+    //Funcion para registar autor
+    public Artista buscarautor(String nom, List<Artista> autores)
+    {
+      for(Artista aut:autores)
+      {
+           if(aut.nombre.equals(nom))
+            return aut;  
+       }
+      return null;
+    }
+
+    public void imprimirautor()
+    {
+        System.out.println("\t Autor: "+this.nombre+"\tAño de Nacimiento: "+this.añoNacimiento);
+    }
+   
+    public void listarautores(List<Artista> autores)
+    {
+    
+        for(Artista aut:autores){
+            System.out.println("\tAutor: "+aut.nombre+"\tAño de Nacimiento: "+aut.añoNacimiento);
+        }
+    }
 }
--- src/github/Github.java
+++ src/github/Github.java
@@ -3,17 +3,125 @@
  * and open the template in the editor.
  */
 package github;
-
+import java.io.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Luisana
  */
-public class Github {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Github 
+{
+    //variables globales
+    public Artista artista;
+    public ArrayList<Artista> autor;
+    public Disco disco;
+    public ArrayList<Disco> discos;
+    public Libro libro;
+    public ArrayList<Libro> libros;    
+    public Pelicula pelicula;
+    public ArrayList<Pelicula> peliculas;
+    
+    public Github()//constructor
+    {
+        artista = new Artista();
+        autor = new ArrayList<Artista>();
+        libro = new Libro();
+        libros = new ArrayList<Libro>();
+        disco = new Disco();
+        discos = new ArrayList<Disco>();
+        pelicula = new Pelicula();
+        peliculas = new ArrayList<Pelicula>();
     }
+    public void main(String[] args) throws IOException
+    {
+        int opc = 0;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader (System.in));
+        
+        while (opc != 5)
+        {
+            System.out.println("***** Menú *****");
+            System.out.println("Seleccione una opción: ");
+            System.out.println(" 1. Artista  ");
+            System.out.println(" 2. Obra");
+            System.out.println(" 3. Película");
+            System.out.println(" 4. Reportes");
+            System.out.println(" 5. Salir");
+            System.out.print(" Ingrese la opción: ");
+            try {
+           
+                opc = Integer.parseInt(entrada.readLine());
+            } catch (IOException ex) {
+                Logger.getLogger(Github.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            switch(opc)
+            {
+                case 1:  //artista
+                    autor_buscar();
+                    break;
+                case 2:  //obra
+                      break;
+                case 3:  // peliculas
+                    break;
+                case 4: // Reportes
+                    break;
+                case 5:
+                    System.out.println("Finalizó");
+                    break;
+                default: 
+                    System.out.println("Opción Errada");
+            }
+        }  
+    }
+    
+    /////////////////////////////////// metodos de seleccion Menus
+     public void autor_buscar() throws IOException
+    {
+        int opc = 0;
+        String nombre;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader (System.in));
+        
+        while (opc != 4)
+        {
+            System.out.println("***** Menu Artista *****");
+            System.out.println(" 1. Registrar Autor  ");
+            System.out.println(" 2. Buscar Autor");
+            System.out.println(" 3. Listar todos los Autores");
+            System.out.println(" 4. Salir");
+            System.out.print(" Ingrese la opción: ");
+
+            try {
+           
+                opc = Integer.parseInt(entrada.readLine());
+            } catch (IOException ex) {
+                Logger.getLogger(Github.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            switch(opc)
+            {
+                case 1: // cargar
+                    artista.registrautor(autor);
+                    break;
+                case 2:// Buscar
+                    System.out.println("Introduzca el nombre del artista a consultar:");
+                    nombre = entrada.readLine();
+                    Artista autorconsultado = artista.buscarautor(nombre, autor);
+                    if(autorconsultado == null)
+                           System.out.println("No se encuentra registrado ese autor.");
+                    else
+                           autorconsultado.imprimirautor();                  
+                    break;
+                case 3: // Listar                   
+                    artista.listarautores(autor);
+                    break;                
+                default: 
+                    System.out.println("Opción Errada");
+            }
+        }  
+    
+    }
+    
+    
 }
